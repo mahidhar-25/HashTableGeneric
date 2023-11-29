c7e2b9195e978f1689438b869e13d2952f706763
mahidhar reddy <mahidhar100008@gmail.com>
2023-11-29 16:12:17 +0530

    Added : initialized the hashtable problem

---
+++ src/com/bridgelabz/hashtable/Main.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.hashtable;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("!!! welcome to hash table program !!! ");
+    }
+}
